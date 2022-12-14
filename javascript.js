361405a2c51e884663ba6b7b79fa2f0f92bf4601
afotohi12 <afotohi12@hotmail.com>
2022-12-14 22:23:32 +0330

    .

--- javascript.js
+++ javascript.js
@@ -2,3 +2,6 @@
 let Title = document.getElementById('Title');
 Title.style.color = 'red';
 Title.style.fontSize = '40px';
+
+let Table = document.getElementsByClassName('table');
+Table[1].style.color = 'blue';
